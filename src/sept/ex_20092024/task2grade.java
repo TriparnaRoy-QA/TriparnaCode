13c0ebf84ba1f9f796350c8a380278f566777833
TRIPARNA ROY <triparna.roy02@gmail.com>
2024-09-28 23:48:46 +0530

    update code task2grade program

--- src/sept/ex_20092024/task2grade.java
+++ src/sept/ex_20092024/task2grade.java
@@ -3,7 +3,7 @@ package sept.ex_20092024;
 public class task2grade {
     public static void main(String[] args){
         int score = 85;
-        String grade=(score>=90) ? "A" :((score>=80)? "B": "C");
+        String grade= score>=80 ? "B": "C";
         System.out.println(grade);
     }
 }
